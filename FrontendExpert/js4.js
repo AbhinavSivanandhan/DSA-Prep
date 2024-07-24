40538f72950fa1dd384ee7b2037cd481e0d1946d
Abhinav S <abhinavstar.anand@gmail.com>
2024-07-24 17:41:57 +0530

    js question

--- FrontendExpert/js4.js
+++ FrontendExpert/js4.js
@@ -1,28 +1,26 @@
-
 class EventTarget {
-    // Write your code here.
-    constructor(){
-      this.listeners={}
-    }
-    addEventListener(name, callback) {
-      if(!this.listeners.hasOwnProperty(name)){
-        this.listeners[name]=new Set([callback]) //same event listener(event listeners of same name) can only be added many times, but only affect once. remaining have no effect
-      } else {
-        this.listeners[name].add(callback);
-      }
-    }
-  
-    removeEventListener(name, callback) {
-      this.listeners[name]?.delete(callback);//checks if name exists, otherwise results to undefined instead of throwing error. else deletes
-    }
-  
-    dispatchEvent(name) {
-      this.listeners[name]?.forEach(callback => {
-        callback();
-      });
+  // Write your code here.
+  constructor(){
+    this.listeners={}
+  }
+  addEventListener(name, callback) {
+    if(!this.listeners.hasOwnProperty(name)){
+      this.listeners[name]=new Set([callback]) //same event listener(event listeners of same name) can only be added many times, but only affect once. remaining have no effect
+    } else {
+      this.listeners[name].add(callback); //allows multiple events listeners for same event(name)
     }
   }
-  
-  // Do not edit the line below.
-  exports.EventTarget = EventTarget;
-  
+
+  removeEventListener(name, callback) {
+    this.listeners[name]?.delete(callback);//checks if name exists, otherwise results to undefined instead of throwing error. else deletes
+  }
+
+  dispatchEvent(name) {
+    this.listeners[name]?.forEach(callback => {
+      callback();
+    });
+  }
+}
+
+// Do not edit the line below.
+exports.EventTarget = EventTarget;
